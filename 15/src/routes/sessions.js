9041fbe4945cfe7aa031d656862da184053a4274
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-22 19:24:56 +0300

    huh

--- 15/src/routes/sessions.js
+++ 15/src/routes/sessions.js
@@ -4,6 +4,25 @@ export default (app) => {
   const users = generateUsers();
 
   // BEGIN (write your solution here)
+  app.get("/sessions/new", (req, res) => {
+    res.view("src/views/sessions/new");
+  });
 
+  app.post("/sessions", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find((user) => user.username === username);
+
+    if (user && decrypt(user.password) === password) {
+      req.session.username = user.username;
+      res.redirect("/");
+    }
+    else {
+      res.view("src/views/sessions/new", { error: "Wrong username or password" });
+    }});
+
+  app.post("/sessions/delete", (req, res) => {
+    req.session.username = null;
+    res.redirect("/");
+  });
   // END
 };
