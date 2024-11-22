3dfcbbbbaeae82b69e17e255df7e5a4e28afcf60
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-22 16:37:30 +0300

    Third trial

--- 3/src/server.js
+++ 3/src/server.js
@@ -8,7 +8,17 @@ export default () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
-
+  app.get('/users', (req, res) => {
+    const { page = 1, per = 5 } = req.query;
+    const currentPage = parseInt(page);
+    const perPage = parseInt(per);
+    const start = (currentPage - 1) * perPage;
+    const end = start + perPage;
+    
+    const pagedUsers = users.slice(start, end);
+    
+    res.send(pagedUsers);
+  });
   // END
 
   return app;
