58be0d938c60eab8bc92243635438d928d817f73
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-22 17:55:20 +0300

    Fifth trial fr now

--- 5/src/server.js
+++ 5/src/server.js
@@ -9,7 +9,20 @@ export default async () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
+  app.register(view, {engine: {pug}});
+  app.get("/users", (req, res) => {
+    res.view("src/views/users/index", { users });
+  })
 
+  app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } 
+    else {
+    res.view("src/views/users/show", { user })};
+  })
   // END
 
   return app;
