e113545332a794eef79ba7d430ef1a219fc6ea77
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-22 17:33:40 +0300

    Fifth trial

--- 4/src/server.js
+++ 4/src/server.js
@@ -7,7 +7,16 @@ export default () => {
   const companies = getCompanies();
 
   // BEGIN (write your solution here)
+  app.get('/companies/:id', (req, res) => {
+    const companyId = req.params.id;
+    const company = companies.find((company) => company.id === companyId);
 
+    if (company) {
+      res.send(company);
+    } else {
+      res.status(404).send("Company not found");
+    }
+  });
   // END
 
   return app;
